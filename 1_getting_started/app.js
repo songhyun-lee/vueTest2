5326f60b133a8ece7849af818b5d091e8d53806e
songhyun-lee <songhyun93lee@gmail.com>
2020-03-27 16:52:14 +0900

    added image url list

--- 1_getting_started/app.js
+++ 1_getting_started/app.js
@@ -28,8 +28,21 @@ const app4 = new Vue({
       { text: "Rick and Morty3" },
       { text: "Rick and Morty4" },
       { text: "Rick and Morty5" },
+    ],
+    images: [
+      "url('https://res.cloudinary.com/du25cd0bj/image/upload/v1579741611/netflixImages/rickAndMorty_ftt3ir.jpg')",
+      'url("https://res.cloudinary.com/du25cd0bj/image/upload/v1579741612/netflixImages/blackmirror2_rq1mjp.png")',
+      'url("https://res.cloudinary.com/du25cd0bj/image/upload/v1579741611/netflixImages/disenchantment_bzsswt.jpg")',
+      'url("https://res.cloudinary.com/du25cd0bj/image/upload/v1579741612/netflixImages/strangerThings_u6vunm.jpg")',
+      'url("https://res.cloudinary.com/du25cd0bj/image/upload/v1579741612/netflixImages/the_OA_unahb8.jpg")',
     ]
-  }
+  },
+  // methods: {
+  //   viewImg: function(e){
+  //     document.querySelector('.projectPreview').style.backgroundImage = this.images[i];
+  //   }
+  // }
+
 })
 
 app4.todos.push({ text: "Disenchantment" });
